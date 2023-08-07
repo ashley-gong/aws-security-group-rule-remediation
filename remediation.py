dccec543f95169139ec9f64c7b12c843122cae4c
ashley-gong <ashley.q.gong@gmail.com>
2023-08-07 13:19:06 -0400

    extended rule to only revoke if port opened to more than 256 addresses

--- remediation.py
+++ remediation.py
@@ -1,16 +1,21 @@
 import boto3
 import botocore
 import json
+import ipaddress
 
 class Event:
-    def __init__(self, group_id=""):
+    def __init__(self, group_id="", from_port=0, to_port=0):
         self.group_id = group_id
+        self.from_port = from_port
+        self.to_port = to_port
 
 # for Lambda handler
 def get_event_details(event):
     e = Event()
-    group_id = event["detail"]["requestParameters"]["groupId"]
-    e.group_id = group_id
+    group = event["detail"]["requestParameters"]
+    e.group_id = group["groupId"]
+    e.from_port = group["ipPermissions"]["items"][0]["fromPort"]
+    e.to_port = group["ipPermissions"]["items"][0]["toPort"]
     return e
 
 # describe security group
@@ -25,17 +30,24 @@ def get_group_info(client, group_id):
 def check_port(from_port, to_port, port_no):
     return port_no >= from_port and from_port <= to_port
 
-def remediate(client, group_id, from_port, to_port, port_no):
+def check_ip_range(cidr):
+    network = ipaddress.IPv4Network(cidr)
+    return network.num_addresses >= 256
+
+def remediate(client, group_id, from_port, to_port, port_no, cidr):
     group = get_group_info(client, group_id)
     if not group:
         return "Security Group Error"
+    if not check_ip_range(cidr):
+        return "Less than 256 IP addresses"
     try:
-        client.revoke_security_group_ingress(
-            CidrIp='0.0.0.0/0',     # 172.31.0.0/16 in EC2?
-            FromPort=from_port,
-            GroupId=group_id,
-            ToPort=to_port,
-            IpProtocol='tcp')
+        if check_port(from_port, to_port, port_no) and check_ip_range(cidr):
+            client.revoke_security_group_ingress(
+                CidrIp=cidr,     # 172.31.0.0/16 in EC2?
+                FromPort=from_port,
+                GroupId=group_id,
+                ToPort=to_port,
+                IpProtocol='tcp')
         
     except botocore.exceptions.ClientError as error:
         return error.response['Error']['Code']
@@ -44,8 +56,8 @@ def lambda_handler(event, context):
     print(event)
     e = get_event_details(event)
     ec2_client = boto3.client('ec2', region_name='us-east-1')
-    remediate(ec2_client, e.group_id, 22)
-    remediate(ec2_client, e.group_id, 3389)
+    remediate(ec2_client, e.group_id, e.from_port, e.to_port, 22, "172.31.0.0/16")
+    remediate(ec2_client, e.group_id, e.from_port, e.to_port, 3389, "172.31.0.0/16")
     group_info = get_group_info(ec2_client, e.group_id)
     print(group_info)
     return {
--- tests.py
+++ tests.py
@@ -26,7 +26,7 @@ def test_1():
              'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
     ])
 
-    ans = remediate(ec2_client, security_group_id, 21, 23, 22)
+    ans = remediate(ec2_client, security_group_id, 21, 23, 22, '0.0.0.0/0')
     assert(ans == None)
     print(get_group_info(ec2_client, security_group_id))
 
@@ -55,7 +55,7 @@ def test_2():
              'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
     ])
 
-    ans = remediate(ec2_client, security_group_id, 3389, 3389, 3389)
+    ans = remediate(ec2_client, security_group_id, 3389, 3389, 3389, '0.0.0.0/0')
     assert(ans == None)
     print(get_group_info(ec2_client, security_group_id))
 
@@ -84,7 +84,7 @@ def test_3():
              'IpRanges': [{'CidrIp': '10.0.0.0/8'}]},
     ])
 
-    ans = remediate(ec2_client, security_group_id, 3389, 3389, 3389)
+    ans = remediate(ec2_client, security_group_id, 3389, 3389, 3389, '0.0.0.0/0')
     assert(ans == 'InvalidPermission.NotFound')
     print(get_group_info(ec2_client, security_group_id))
 
@@ -113,7 +113,7 @@ def test_4():
              'IpRanges': [{'CidrIp': '0.0.0.0/0'}, {'CidrIp': '10.0.0.0/8'}]},
     ])
 
-    ans = remediate(ec2_client, security_group_id, 22, 22, 22)
+    ans = remediate(ec2_client, security_group_id, 22, 22, 22, '0.0.0.0/0')
     assert(ans == None)
     print(get_group_info(ec2_client, security_group_id))
 
@@ -146,8 +146,8 @@ def test_5():
               'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
     ])
 
-    ans = remediate(ec2_client, security_group_id, 22, 22, 22)
-    ans2 = remediate(ec2_client, security_group_id, 3387, 3390, 3389)
+    ans = remediate(ec2_client, security_group_id, 22, 22, 22, '0.0.0.0/0')
+    ans2 = remediate(ec2_client, security_group_id, 3387, 3390, 3389, '0.0.0.0/0')
     assert(ans == None)
     assert(ans2 == None)
     print(get_group_info(ec2_client, security_group_id))
@@ -181,8 +181,37 @@ def test_6():
         GroupId=security_group_id
     )
 
-    ans = remediate(ec2_client, security_group_id, 22, 22, 22)
+    ans = remediate(ec2_client, security_group_id, 22, 22, 22, '0.0.0.0/0')
     assert(ans == "Security Group Error")
     print(ans)
 
 test_6()
+
+# Revoke Port 22 ingress - less than 256 addresses, won't revoke
+@mock_ec2
+def test_7():
+    ec2_client = boto3.client('ec2', region_name='us-east-1')
+    
+    # Create security group
+    response = ec2_client.create_security_group(
+        GroupName='Test7',
+        Description='Port 22 open to 152.2.136.0/26 - rule should not be revoked',
+    )
+
+    security_group_id = response['GroupId']
+
+    # Authorize security group ingress
+    ec2_client.authorize_security_group_ingress(
+        GroupId=security_group_id,
+        IpPermissions=[
+            {'IpProtocol': 'tcp',
+             'FromPort': 21,
+             'ToPort': 23,
+             'IpRanges': [{'CidrIp': '152.2.136.0/26'}]},
+    ])
+
+    ans = remediate(ec2_client, security_group_id, 21, 23, 22, '152.2.136.0/26')
+    assert(ans == "Less than 256 IP addresses")
+    print(ans)
+
+test_7()
